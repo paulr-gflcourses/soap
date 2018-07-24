af5d812613cd448f967b7e8ba35627928b5930d0
paulr_gfl12 <paulriabov@gmail.com>
2018-07-24 21:28:48 +0300

    task1 dev

--- task1/index.php
+++ task1/index.php
@@ -5,79 +5,60 @@ function serviceSoap1()
 {
     $url="https://www.w3schools.com/xml/tempconvert.asmx?WSDL";
     $client = new SoapClient($url);
-
-    var_dump($client->__getFunctions());
+    //var_dump($client->__getFunctions());
     $result = $client->CelsiusToFahrenheit(array('Celsius' => '10'));
-
     echo $result->CelsiusToFahrenheitResult . "\n";
-}
-
-function serviceCurl()
-{
-    $url="https://www.w3schools.com/xml/tempconvert.asmx?WSDL";
-    $ch = curl_init($url);
-
-    //$fp = fopen("example_homepage.txt", "w");
-    //curl_setopt($ch, CURLOPT_FILE, $fp);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-
-    curl_exec($ch);
-    curl_close($ch);
-    fclose($fp);
-    return false;
 }
 
 function serviceSoap2()
 {
-    //$url="https://www.w3schools.com/xml/tempconvert.asmx?WSDL";
-    //$url="http://webservices.amazon.com/AWSECommerceService/AWSECommerceService.wsdl";
-    $url="https://demo.myalm.ru/api/securityservice?wsdl";
-    $url="https://demo.myalm.ru/api/dataservice?wsdl";
-    $data =  array( 'soap_version'=>SOAP_1_1, 'cache_wsdl' => WSDL_CACHE_NONE, 'trace'=>true );
-
-    $client = new SoapClient($url,$data);
-
-    var_dump($client->__getFunctions());
-
-    //$result = $client->RemoteGetAll(array('Celsius' => '10'));
-
-    //$security = new SoapClient(
-        //'https://demo.myalm.ru/api/securityservice?wsdl',
-        //array(
-            //'soap_version'=>SOAP_1_1,
-            //'cache_wsdl' => WSDL_CACHE_NONE,
-            //'trace'=>true
-
-        //)
-
-    //);
-    //$result = $security->login(
-        //array(
-            //'username' => '*****',
-            //'userpass' => '****',
-            //'project' => '****'
-        //)
-
-    //);
-    //$token = $result->return;
-    //var_dump($token);
-
-
-}
-
-function serviceSoap3()
-{
     $url="http://www.dataaccess.com/webservicesserver/numberconversion.wso?WSDL";
     $client = new SoapClient($url);
-
-    //var_dump($client->__getFunctions());
     $result = $client->NumberToWords(array('ubiNum' => '2931'));
     echo $result->NumberToWordsResult;
 }
 
+function serviceSoap3()
+{
+    $url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL";
+    $client = new SoapClient($url);
+    var_dump($client->__getFunctions());
+    //$result = $client->ListOfContinentsByName();
+    $result = $client->ListOfCountryNamesByName();
+    $result = $result->ListOfCountryNamesByNameResult->tCountryCodeAndName;
+    var_dump($result);
+    print $result[0]->sName;
+    //$res2 = $result->ListOfContinentsByNameResult->tContinent;
+    //var_dump($res2);
+}
+
+function serviceCurl()
+{
+    $url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso";
+    //$methodName = '/ListOfContinentsByName';
+    $methodName = '/ListOfCountryNamesByName';
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_URL, $url.$methodName);
+    $output = curl_exec($ch); 
+    print gettype($output);
+    curl_close($ch);  
+
+    //var_dump($output);
+    //$oXML = new SimpleXMLElement($output);
+
+    //$oXML = simplexml_load_string($output, 'SimpleXMLElement', LIBXML_NOCDATA);
+    //var_dump($oXML);
+    //foreach($oXML->entry as $oEntry){
+        ////echo $oEntry->title . "\n";
+        //var_dump($oEntry);
+
+    //}
+}
+
 //serviceSoap1();
 //serviceSoap2();
-serviceSoap3();
+//serviceSoap3();
+serviceCurl();
 
 //include_once TEMPLATE;
 
